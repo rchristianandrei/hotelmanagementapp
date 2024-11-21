e15ced71292414d6f768022e9d388b7411c2fcbd
Andrei Reyes <rchristianandrei@gmail.com>
2024-11-22 01:51:31 +0800

    create search field

---
+++ Hotmap/Hotmap/Hotmap/UserControls/SearchField.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SearchField
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim CustomizableEdges1 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges2 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges3 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges4 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        txtSearch = New Guna.UI2.WinForms.Guna2TextBox()
+        btnSearch = New Guna.UI2.WinForms.Guna2Button()
+        SuspendLayout()
+        ' 
+        ' txtSearch
+        ' 
+        txtSearch.BorderRadius = 6
+        txtSearch.CustomizableEdges = CustomizableEdges1
+        txtSearch.DefaultText = ""
+        txtSearch.DisabledState.BorderColor = Color.FromArgb(CByte(208), CByte(208), CByte(208))
+        txtSearch.DisabledState.FillColor = Color.FromArgb(CByte(226), CByte(226), CByte(226))
+        txtSearch.DisabledState.ForeColor = Color.FromArgb(CByte(138), CByte(138), CByte(138))
+        txtSearch.DisabledState.PlaceholderForeColor = Color.FromArgb(CByte(138), CByte(138), CByte(138))
+        txtSearch.Dock = DockStyle.Fill
+        txtSearch.FocusedState.BorderColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        txtSearch.Font = New Font("Segoe UI", 9F)
+        txtSearch.HoverState.BorderColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        txtSearch.Location = New Point(0, 0)
+        txtSearch.Name = "txtSearch"
+        txtSearch.PasswordChar = ChrW(0)
+        txtSearch.PlaceholderText = "Search..."
+        txtSearch.SelectedText = ""
+        txtSearch.ShadowDecoration.CustomizableEdges = CustomizableEdges2
+        txtSearch.Size = New Size(605, 44)
+        txtSearch.TabIndex = 9
+        ' 
+        ' btnSearch
+        ' 
+        btnSearch.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        btnSearch.BackColor = Color.White
+        btnSearch.BorderRadius = 10
+        btnSearch.CustomizableEdges = CustomizableEdges3
+        btnSearch.DisabledState.BorderColor = Color.DarkGray
+        btnSearch.DisabledState.CustomBorderColor = Color.DarkGray
+        btnSearch.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        btnSearch.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        btnSearch.FillColor = Color.Fuchsia
+        btnSearch.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnSearch.ForeColor = Color.White
+        btnSearch.Location = New Point(527, 3)
+        btnSearch.Name = "btnSearch"
+        btnSearch.ShadowDecoration.CustomizableEdges = CustomizableEdges4
+        btnSearch.Size = New Size(75, 38)
+        btnSearch.TabIndex = 12
+        btnSearch.Text = "Search"
+        ' 
+        ' SearchField
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Transparent
+        Controls.Add(btnSearch)
+        Controls.Add(txtSearch)
+        Name = "SearchField"
+        Size = New Size(605, 44)
+        ResumeLayout(False)
+    End Sub
+
+    Private WithEvents txtSearch As Guna.UI2.WinForms.Guna2TextBox
+    Private WithEvents btnSearch As Guna.UI2.WinForms.Guna2Button
+
+End Class
---
+++ Hotmap/Hotmap/Hotmap/UserControls/SearchField.vb
@@ -0,0 +1,21 @@
+﻿Public Class SearchField
+#Region "Properties"
+    Public Property Placeholder() As String
+        Get
+            Return Me.txtSearch.PlaceholderText
+        End Get
+        Set(ByVal value As String)
+            Me.txtSearch.PlaceholderText = value
+        End Set
+    End Property
+#End Region
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.btnSearch.FillColor = Theme.PrimaryColor
+    End Sub
+End Class

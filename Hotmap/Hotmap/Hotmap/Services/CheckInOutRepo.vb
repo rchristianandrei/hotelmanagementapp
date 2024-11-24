c7c9f0fbc9a1fc7c35dcde2a8be76219c93569d8
Andrei Reyes <rchristianandrei@gmail.com>
2024-11-24 22:44:38 +0800

    apply token on checkinout requests

--- Hotmap/Hotmap/Hotmap/Services/CheckInOutRepo.vb
+++ Hotmap/Hotmap/Hotmap/Services/CheckInOutRepo.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Text.Json.Nodes
+﻿Imports System.Net.Http
+Imports System.Text.Json.Nodes
 Imports MySql.Data.MySqlClient
 
 Public Class CheckInOutRepo
@@ -8,26 +9,22 @@ Public Class CheckInOutRepo
 #Region "Create / Update"
     Public Async Function HttpPost(roomId As Integer, hour As Integer) As Task(Of String)
         Dim apiUrl = Me.url & $"/{roomId}/{hour}"
-        Dim response = Await Services.HttpClient.PostAsync(apiUrl, Nothing)
+
+        Dim request As New HttpRequestMessage(HttpMethod.Post, apiUrl)
+        request.Headers.Add("Authorization", Services.AuthToken)
+
+        Dim response = Await Services.HttpClient.SendAsync(request)
         response.EnsureSuccessStatusCode()
         Return Await response.Content.ReadAsStringAsync()
     End Function
-
-    'Public Async Function Save(roomId As Integer, hour As Integer) As Task
-    '    Dim query = $"INSERT INTO tblCheckInOut(id, dCheckOut)
-    '                VALUES({roomId}, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL {hour} HOUR));"
-    '    Using conn As New MySqlConnection(Me.connectionString)
-    '        Using command As New MySqlCommand(query, conn)
-    '            Await conn.OpenAsync()
-    '            Await command.ExecuteNonQueryAsync
-    '        End Using
-    '    End Using
-    'End Function
 #End Region
 
 #Region "Read"
     Public Async Function HttpGet1000() As Task(Of List(Of CheckInOutModel))
-        Dim response = Await Services.HttpClient.GetAsync(Me.url)
+        Dim request As New HttpRequestMessage(HttpMethod.Get, Me.url)
+        request.Headers.Add("Authorization", Services.AuthToken)
+
+        Dim response = Await Services.HttpClient.SendAsync(request)
         response.EnsureSuccessStatusCode()
 
         Dim content = Await response.Content.ReadAsStringAsync
@@ -46,46 +43,17 @@ Public Class CheckInOutRepo
 
         Return list
     End Function
-
-    Public Async Function Get1000() As Task(Of List(Of CheckInOutModel))
-        Dim query = "SELECT id, dCheckIn, dCheckOut 
-                    FROM tblCheckInOut
-                    LIMIT 1000;"
-        Dim list As New List(Of CheckInOutModel)
-        Using conn As New MySqlConnection(Me.connectionString)
-            Using command As New MySqlCommand(query, conn)
-                Await conn.OpenAsync()
-                Using reader = Await command.ExecuteReaderAsync
-                    While reader.Read
-                        list.Add(New CheckInOutModel With {
-                            .ID = reader("id"),
-                            .CheckIn = reader("dCheckIn"),
-                            .CheckOut = reader("dCheckOut")
-                        })
-                    End While
-                End Using
-            End Using
-        End Using
-        Return list
-    End Function
 #End Region
 
 #Region "Delete"
     Public Async Function HttpDelete(roomID As Integer) As Task(Of String)
         Dim apiUrl = Me.url & $"/{roomID}"
-        Dim response = Await Services.HttpClient.DeleteAsync(apiUrl)
+        Dim request As New HttpRequestMessage(HttpMethod.Delete, apiUrl)
+        request.Headers.Add("Authorization", Services.AuthToken)
+
+        Dim response = Await Services.HttpClient.SendAsync(request)
         response.EnsureSuccessStatusCode()
         Return Await response.Content.ReadAsStringAsync()
     End Function
-
-    'Public Async Function Delete(checkInOutID As Integer) As Task
-    '    Dim query = $"DELETE FROM tblCheckInOut WHERE id = {checkInOutID};"
-    '    Using conn As New MySqlConnection(Me.connectionString)
-    '        Using command As New MySqlCommand(query, conn)
-    '            Await conn.OpenAsync()
-    '            Await command.ExecuteNonQueryAsync
-    '        End Using
-    '    End Using
-    'End Function
 #End Region
 End Class
